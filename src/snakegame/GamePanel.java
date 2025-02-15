6fc5fc77eda39bc68caed3b6cc2827856abea69f
GosuCode <gosucode1945@gmail.com>
2025-02-15 11:29:28 +0545

    feat: define essential game methods for rendering and movements

--- src/snakegame/GamePanel.java
+++ src/snakegame/GamePanel.java
@@ -1,13 +1,56 @@
 package snakegame;
 
 import javax.swing.JPanel;
+
+import java.awt.Graphics;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 class GamePanel extends JPanel implements ActionListener {
+	
+	public GamePanel() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void startGame() {
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		
+	}
+	
+	public void draw(Graphics g) {
+		
+	}
+	
+	public void move() {
+		
+	}
+	
+	public void checkTarget() {
+		
+	}
+	
+	public void checkCollisions() {
+		
+	}
+	
+	public void gameOver(Graphics g) {
+		
+	}
 
   @Override
   public void actionPerformed(java.awt.event.ActionEvent e) {
     System.out.println("Action Performed");
+  }
+  
+  public class MyKeyAdapter extends KeyAdapter{
+	  @Override
+	  public void keyPressed(KeyEvent e) {
+		  
+	  }
   }
 
 }
