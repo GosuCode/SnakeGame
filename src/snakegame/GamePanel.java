b72eaed10cbbfd772c3e4360d82f69188bb9e285
GosuCode <gosucode1945@gmail.com>
2025-02-15 11:40:45 +0545

    chore: declare game-related variables

--- src/snakegame/GamePanel.java
+++ src/snakegame/GamePanel.java
@@ -6,11 +6,28 @@ import java.awt.Graphics;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.Random;
+import java.util.Timer;
 
 class GamePanel extends JPanel implements ActionListener {
 	
+	static final int SCREEN_WIDTH = 600;
+	static final int SCREEN_HEIGHT = 600;
+	static final int UNIT_SIZE = 25;
+	static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT)/ UNIT_SIZE;
+	static final int DELAY = 75; // Delay for timer
+	final int x[] = new int[GAME_UNITS];
+	final int y[] = new int[GAME_UNITS];
+	int snakeLength = 6;
+	int targetKilled;
+	int targetX;
+	int targetY;
+	char direction = 'R'; // direction where snakes will go when game starts
+	boolean running = false;
+	Timer timer;
+	Random random;
+	
 	public GamePanel() {
-		// TODO Auto-generated constructor stub
 	}
 	
 	public void startGame() {
