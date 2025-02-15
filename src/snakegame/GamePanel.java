1e5982d7d0ab8e10835b2fb66692ff8369415860
GosuCode <gosucode1945@gmail.com>
2025-02-15 15:01:08 +0545

    feat: add Game Over screen and display final score

--- src/snakegame/GamePanel.java
+++ src/snakegame/GamePanel.java
@@ -5,6 +5,8 @@ import javax.swing.Timer;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
@@ -59,23 +61,33 @@ class GamePanel extends JPanel implements ActionListener {
 
 	public void draw(Graphics g) {
 
-		for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
-			g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
-			g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
-		}
-
-		g.setColor(Color.red);
-		g.fillOval(targetX, targetY, UNIT_SIZE, UNIT_SIZE);
-
-		// Snake
-		for (int i = 0; i < snakeLength; i++) {
-			if (i == 0) {
-				g.setColor(Color.green);
-				g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
-			} else {
-				g.setColor(new Color(45, 180, 0));
-				g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+		if (running) {
+			for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
+				g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+				g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
 			}
+
+			g.setColor(Color.red);
+			g.fillOval(targetX, targetY, UNIT_SIZE, UNIT_SIZE);
+
+			// Snake
+			for (int i = 0; i < snakeLength; i++) {
+				if (i == 0) {
+					g.setColor(Color.green);
+					g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+				} else {
+					g.setColor(new Color(45, 180, 0));
+					g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+				}
+			}
+			
+		g.setColor(Color.red);
+		g.setFont(new Font("Verdana", Font.BOLD, 20));
+		FontMetrics metrics = getFontMetrics(g.getFont());
+		g.drawString("Score: " + targetKilled, (SCREEN_WIDTH - metrics.stringWidth("Score: " + targetKilled))/2, g.getFont().getSize());
+
+		} else {
+			gameOver(g);
 		}
 
 	}
@@ -114,7 +126,7 @@ class GamePanel extends JPanel implements ActionListener {
 	}
 
 	public void checkTarget() {
-		if((x[0] == targetX) && (y[0] == targetY)) {
+		if ((x[0] == targetX) && (y[0] == targetY)) {
 			snakeLength++;
 			targetKilled++;
 			newTarget();
@@ -122,41 +134,49 @@ class GamePanel extends JPanel implements ActionListener {
 	}
 
 	public void checkCollisions() {
-		
+
 		// checks if head collides with body
-		for(int i = snakeLength; i>0; i--) {
-			if((x[0] == x[i]) && (y[0] == y[i])) {
+		for (int i = snakeLength; i > 0; i--) {
+			if ((x[0] == x[i]) && (y[0] == y[i])) {
 				running = false;
 			}
 		}
-		
+
 		// left border
-		if(x[0] < 0) {
+		if (x[0] < 0) {
 			running = false;
 		}
-		
+
 		// right border
-		if(x[0] > SCREEN_WIDTH) {
+		if (x[0] > SCREEN_WIDTH) {
 			running = false;
 		}
-		
+
 		// top border
-		if(y[0] < 0) {
+		if (y[0] < 0) {
 			running = false;
 		}
 
 		// bottom border
-		if(y[0] > SCREEN_HEIGHT) {
+		if (y[0] > SCREEN_HEIGHT) {
 			running = false;
 		}
-		
-		if(running == false) {
+
+		if (running == false) {
 			timer.stop();
 		}
 	}
 
 	public void gameOver(Graphics g) {
-
+		g.setColor(Color.red);
+		g.setFont(new Font("Ink Free", Font.BOLD, 75));
+		FontMetrics metrics1 = getFontMetrics(g.getFont());
+		g.drawString("Game Over", (SCREEN_WIDTH - metrics1.stringWidth("Game Over"))/2, SCREEN_HEIGHT/2);
+		
+		g.setColor(Color.red);
+		g.setFont(new Font("Verdana", Font.BOLD, 30));
+		FontMetrics metrics2 = getFontMetrics(g.getFont());
+		g.drawString("Score: " + targetKilled, (SCREEN_WIDTH - metrics2.stringWidth("Score: " + targetKilled))/2, g.getFont().getSize());
 	}
 
 	@Override
